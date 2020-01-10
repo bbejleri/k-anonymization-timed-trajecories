35249996b0ad05ca7c65a2a244014ded5a4f802d
User <User@DESKTOP-B7JDSTE>
2020-01-10 06:13:52 -0800

    added trajectory without noise for single macs

--- bora.thesis/src/main/java/com/bora/thesis/core/TrajectoryRecordController.java
+++ bora.thesis/src/main/java/com/bora/thesis/core/TrajectoryRecordController.java
@@ -1,6 +1,8 @@
 package com.bora.thesis.core;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,9 +26,20 @@ public class TrajectoryRecordController {
 	@RequestMapping(value = "/routes/{hashmac}", method = RequestMethod.GET)
 	public String doGetRouts(final Model model, @PathVariable("hashmac") String hashmac) {
 		List<SingleRecord> routes = this.singleRecordService.getByMacAddress(hashmac);
+		List<String> locations = routes.stream().map(x -> x.getZone()).distinct().collect(Collectors.toList());
+		List<SingleRecord> selectedRoutes = new ArrayList<SingleRecord>();
+		locations.stream().forEach(l -> {
+			routes.stream().filter(x -> x.getZone() == l).findFirst().ifPresent(selectedRoutes::add);
+		});
+		StringBuilder sb = new StringBuilder();
+		String finaltrajectory = null;
+		for (SingleRecord record : selectedRoutes) {
+			finaltrajectory = sb.append(" --> ").append(record.getZone()).append(" (").append(record.getTimestamp()).append(") ").toString();
+		}
 		model.addAttribute("totalentries", routes.size());
 		model.addAttribute("distinctzones", routes.stream().map(x -> x.getZone()).distinct().count());
 		model.addAttribute("distinctmacs", routes.stream().map(x -> x.getHashMac()).distinct().count());
+		model.addAttribute("finaltrajectory", finaltrajectory);
 		model.addAttribute("mac", hashmac);
 		model.addAttribute("list", routes);
 		return "trajectory-record-view";
